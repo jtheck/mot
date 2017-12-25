99e95b4bb255dc2bd61990a8e7f50b3ea413512d
jtheck <cb.monk@gmail.com>
2017-12-25 15:52:54 -0700

    s

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
+const port = process.env.PORT;
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => {console.log('Example app listening on port '+ port +'!')})
