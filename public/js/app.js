1664135de0121088a40b3a9672f19c3cc231d549
jtheck <cb.monk@gmail.com>
2019-08-22 19:48:37 -0700

    add sw

--- public/js/app.js
+++ public/js/app.js
@@ -20,6 +20,22 @@ var init = function() {
 
   // joe.setSense("Text Input", "text_out");
 
+  
+// Check compatibility for the browser we're running this in
+if ("serviceWorker" in navigator) {
+  if (navigator.serviceWorker.controller) {
+    console.log("sw active service worker found, no need to register");
+  } else {
+    // Register the service worker
+    navigator.serviceWorker
+      .register("sw-momo.js", {
+        scope: "./"
+      })
+      .then(function (reg) {
+        console.log("sw Service worker has been registered for scope: " + reg.scope);
+      });
+  }
+}
 
 
   var chat = GETFIRE({topicName: "mot moe", startOpen: false, fullHeight: false, devMode: false});
