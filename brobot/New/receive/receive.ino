562466e3130d9bdf17b318257c085de03740925c
Heck <justintheck@gmail.com>
2022-08-28 22:57:56 -0700

    save

--- brobot/New/receive/receive.ino
+++ brobot/New/receive/receive.ino
@@ -1,6 +1,5 @@
 // RX
 #include <RH_ASK.h>
-//#include <SPI.h>
 
 RH_ASK driver(2000,14,12,0);
 const int out = 9;
@@ -8,30 +7,30 @@ char receive[32];
 
 void setup()
 {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
   driver.init();
   pinMode(out, OUTPUT);
+  
   Serial.println("rdy!");
 }
 
 void loop()
 {
-  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
-  uint8_t buflen = sizeof(buf);
+  int dat[3]={0};
+  
+  uint8_t buflen = sizeof(dat);
 
-  if (driver.recv(buf, &buflen))
+  if (driver.recv((uint8_t*)dat, &buflen))
   {
-    memset(receive, 0, sizeof(receive));
-    for (int i = 0; i < buflen; i++) {
-      receive[i] = buf[i];
+    for (int i=0; i<3; i++){
+      Serial.println(dat[i]);
     }
-    if (strcmp(receive, "Switch_ON") == 0) {
+    
+    if (dat[1] == 1 || dat[2] == 1) {
       digitalWrite(out, HIGH);
-      Serial.println(receive);
-    } else if (strcmp(receive, "Switch_OFF") == 0) {
+    } else {
       digitalWrite(out, LOW);
-    Serial.println(receive);
     }
     
   }
--- brobot/New/transmit/transmit.ino
+++ brobot/New/transmit/transmit.ino
@@ -2,13 +2,10 @@
 #include <RH_ASK.h>
 
 
-
 RH_ASK driver(2000, 14, 12, 0);
 
 const int switch_on = 9, switch_off = 10;
-int state = 0;
-char *msg;
-
+int dat[3];
 
 void setup()
 {
@@ -18,31 +15,20 @@ void setup()
 
   pinMode(switch_on, INPUT);
   pinMode(switch_off, INPUT);
-
 }
 
 void loop()
 {
+  
+  dat[0] = analogRead(A0);
+  dat[1]=0;
+  dat[2]=0;
   if (digitalRead(switch_on) == HIGH){
-    msg = "Switch_ON";
-    state = 1;
+    dat[1] = 1;
   }
-  else if (digitalRead(switch_off) == HIGH){
-    msg = "Switch_OFF";
-    state = 1;
+  if (digitalRead(switch_off) == HIGH){
+    dat[2] = 1;
   }
-  else if (state == 1){
-    driver.send((uint8_t *)msg, strlen(msg));
-    driver.waitPacketSent();
-    Serial.println("SENT ");
-//    Serial.println((uint8_t *)msg);
-//    delay(200);
-    
-    state = 0;
-  }
-//  char msg = "hello mot.moe! "+String(iter);
-//  Serial.println(msg);
-//    driver.send((uint8_t *)msg, strlen(msg));
-//  driver.waitPacketSent();
-//  delay(500);
+  driver.send((uint8_t *)&dat, sizeof(dat));
+  driver.waitPacketSent();
 }
