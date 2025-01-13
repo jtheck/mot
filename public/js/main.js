18fda81bccf15da8d13ef42ebf9839faacccee95
heck <justintheck@gmail.com>
2025-01-13 02:09:42 -0800

    deploy

--- public/js/main.js
+++ public/js/main.js
@@ -6,17 +6,17 @@ var init = function(){
   initTraining();
   initControl();
 
-  // Init chat
-  if (typeof GETFIRE !== "undefined"){
-    var chat = GETFIRE({topicNames: ["mot moe", 'test'],
-      defaultName: Math.random() > .5 ? "Beep" : "Boop",
-      startOpen: false,
-      startPreview: false,
-      clickAwayHide: false,
-      mouseOutFade: true,
-      titleAlerts: true,
-      topCorner: false,
-      devMode: (window.location.protocol != 'https:')});
-  }
+  // // Init chat
+  // if (typeof GETFIRE !== "undefined"){
+  //   var chat = GETFIRE({topicNames: ["mot moe", 'test'],
+  //     defaultName: Math.random() > .5 ? "Beep" : "Boop",
+  //     startOpen: false,
+  //     startPreview: false,
+  //     clickAwayHide: false,
+  //     mouseOutFade: true,
+  //     titleAlerts: true,
+  //     topCorner: false,
+  //     devMode: (window.location.protocol != 'https:')});
+  // }
 
 }
